11cee7b443e36d5eb06f4ee2c6dd456f5cef4922
zhangjintao233 <57163295+zhangjintao233@users.noreply.github.com>
2020-01-18 09:12:33 +0800

    Initial commit

---
+++ rmsjw/javawebdemo/src/com/Test.java
@@ -0,0 +1,9 @@
+package com;
+
+public class Test {
+    public static void main(String[] args) {
+        String s1 = "ABCD";
+        String s2 = "1234";
+        System.out.println(s1 + s2);
+    }
+}
